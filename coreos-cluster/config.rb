3e4ed2d4f259881d3a53ac167897681aaacc8f61
Tim Allen <tiallen@softlayer.com>
2014-11-12 10:24:42 -0600

    [Tim] Setup ansible provisioning for coreos cluster.

--- coreos-cluster/config.rb
+++
@@ -1,8 +0,0 @@
-require 'open-uri'
-require 'yaml'
-
-token = open('https://discovery.etcd.io/new').read
-data = YAML.load(IO.readlines('cloud-config-template.yml')[1..-1].join)
-data['coreos']['etcd']['discovery'] = token
-yaml = YAML.dump(data)
-File.open('cloud-config.yml', 'w') { |file| file.write("#cloud-config\n\n#{yaml}") }
